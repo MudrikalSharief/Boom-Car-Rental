0d7ba666152cdd23bd7f0a85bd696094988b333e
coffee-Maker19 <125376601+coffee-Maker19@users.noreply.github.com>
2024-04-17 23:34:54 +0800

    so far so goood

--- javascipt/script.js
+++ javascipt/script.js
@@ -1,9 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
     const activebutton = document.getElementsByClassName('active-button')[0];
+    const nav1 = document.getElementsByClassName('nav_a')[0];
     const navul = document.getElementsByClassName('navul')[0];
   
     activebutton.addEventListener('click', () => {
         navul.classList.toggle('active');
     });
+    navul.addEventListener('click', () => {
+        navul.classList.toggle('active');
+    });
+
   });
   
