254535cbe3e30b86b027976c2aa9c4026f044eaa
coffee-Maker19 <125376601+coffee-Maker19@users.noreply.github.com>
2024-04-19 00:32:59 +0800

    greatest change of all time

--- javascipt/script.js
+++ javascipt/script.js
@@ -1,6 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
     const activebutton = document.getElementsByClassName('active-button')[0];
-    const nav1 = document.getElementsByClassName('nav_a')[0];
     const navul = document.getElementsByClassName('navul')[0];
   
     activebutton.addEventListener('click', () => {
