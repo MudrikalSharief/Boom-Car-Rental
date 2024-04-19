d5b7e869debd74f7e015d27b29d4d8d94ea4154a
coffee-Maker19 <125376601+coffee-Maker19@users.noreply.github.com>
2024-04-19 09:09:36 +0800

    upgraded shii

---
+++ user/javascipt/script.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const activebutton = document.getElementsByClassName('active-button')[0];
+    const navul = document.getElementsByClassName('navul')[0];
+  
+    activebutton.addEventListener('click', () => {
+        navul.classList.toggle('active');
+    });
+    navul.addEventListener('click', () => {
+        navul.classList.toggle('active');
+    });
+
+  });
+  
